f47d925f69000a67bee527ea1cf60eed865d3229
ITZM33X <67522253+ITZM33X@users.noreply.github.com>
2023-02-20 13:19:36 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local UIVersion = "0.3.8"
+local UIVersion = "0.3.9"
 local mouse = game.Players.LocalPlayer:GetMouse()	
 local uis = game:GetService("UserInputService")
 local tweenservice = game:GetService("TweenService")
@@ -52,7 +52,7 @@ function Lib:CreateMain(Name)
 	local TabHolder = Instance.new("Frame")
 	local TabHolderPadding = Instance.new("UIPadding")
 	local TabHolderLayout = Instance.new("UIListLayout")
-	
+
 	MadUI.Name = "MadUI"
 	MadUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
@@ -114,38 +114,42 @@ function Lib:CreateMain(Name)
 			true
 		)
 	end)
-	
+
 	uis.InputBegan:Connect(function(Key)
 		if Key.KeyCode == Enum.KeyCode.F5 then
 			if open then
 				open = false
-				Main:TweenPosition(UDim2.new(-0.25, 0,0.5, 0),
-					Enum.EasingDirection.Out,
-					Enum.EasingStyle.Sine,
-					0.2,
-					true
-				)
+				pcall(function()
+					Main:TweenPosition(UDim2.new(-0.25, 0,0.5, 0),
+						Enum.EasingDirection.Out,
+						Enum.EasingStyle.Sine,
+						0.2,
+						true
+					)
+				end)
 			else
 				open = true
-				Main:TweenPosition(UDim2.new(0.5, 0,0.5, 0),
-					Enum.EasingDirection.Out,
-					Enum.EasingStyle.Sine,
-					0.2,
-					true
-				)
+				pcall(function()
+					Main:TweenPosition(UDim2.new(0.5, 0,0.5, 0),
+						Enum.EasingDirection.Out,
+						Enum.EasingStyle.Sine,
+						0.2,
+						true
+					)
+				end)
 			end
 		end
 	end)
-	
-	
-	
-	
-	
+
+
+
+
+
 	TopBarCorner.CornerRadius = UDim.new(0.100000001, 0)
 	TopBarCorner.Name = "TopBarCorner"
 	TopBarCorner.Parent = TopBar
-	
-	
+
+
 	Navigation.Name = "Navigation"
 	Navigation.Parent = Main
 	Navigation.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
@@ -177,15 +181,15 @@ function Lib:CreateMain(Name)
 
 	UIPadding_2.Parent = GameNAme
 	UIPadding_2.PaddingLeft = UDim.new(0.0299999993, 0)
-	
+
 	local PLACEID = game.PlaceId
 
 	local Product = game:GetService("MarketplaceService")
 
 	local Details = Product:GetProductInfo(PLACEID)
 	local IconID = Details.IconImageAssetId
-	
-	
+
+
 	GameImage.Name = "GameImage"
 	GameImage.Parent = GameFrame
 	GameImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -201,7 +205,7 @@ function Lib:CreateMain(Name)
 	ButtonHolder.BackgroundTransparency = 1.000
 	ButtonHolder.ClipsDescendants = true
 	ButtonHolder.Size = UDim2.new(1, 0, 0.853796363, 0)
-	
+
 	ButtonHolderLayout.Name = "ButtonHolderLayout"
 	ButtonHolderLayout.Parent = ButtonHolder
 	ButtonHolderLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
@@ -222,16 +226,16 @@ function Lib:CreateMain(Name)
 	TabHolder.ClipsDescendants = true
 	TabHolder.Position = UDim2.new(0.239999995, 0, 0.0975164771, 0)
 	TabHolder.Size = UDim2.new(0.759673178, 0, 0.898483694, 0)
-	
-	
+
+
 	local firstPage
-	
+
 	local Tab = {}
 	function Tab:CreateTab(Name)	
 		local TabHolderTabElement = Instance.new("ScrollingFrame")
 		local TabHolderPadding = Instance.new("UIPadding")
 		local TabHolderLayout = Instance.new("UIListLayout")
-		
+
 		TabHolderTabElement.Name = Name
 		TabHolderTabElement.Parent = TabHolder
 		TabHolderTabElement.Active = true
@@ -252,19 +256,19 @@ function Lib:CreateMain(Name)
 		TabHolderLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 		TabHolderLayout.SortOrder = Enum.SortOrder.LayoutOrder
 		TabHolderLayout.Padding = UDim.new(0.00499999989, 0)
-		
-		
+
+
 		local ButtonHolderElement = Instance.new("Frame")
 		local ElementUiCorne = Instance.new("UICorner")
 		local ButtonHolderElementEdge = Instance.new("Frame")
 		local ButtonHolderPage = Instance.new("TextButton")
-		
+
 		ButtonHolderElement.Name = Name.."Btn"
 		ButtonHolderElement.Parent = ButtonHolder
 		ButtonHolderElement.BackgroundColor3 = Color3.fromRGB(57, 57, 57)
 		ButtonHolderElement.Position = UDim2.new(-3.38150983e-07, 0, 0.105000019, 0)
 		ButtonHolderElement.Size = UDim2.new(0.877995789, 0, 0.100000001, 0)
-		
+
 		if firstPage == nil then
 			firstPage = ButtonHolderElement
 		end
@@ -292,8 +296,8 @@ function Lib:CreateMain(Name)
 		ButtonHolderPage.TextSize = 14.000
 		ButtonHolderPage.TextWrapped = true
 		ButtonHolderPage.Text = Name
-		
-		
+
+
 		ButtonHolderPage.MouseButton1Click:Connect(function()
 			for i,v in pairs(ButtonHolder:GetChildren()) do
 				if v:IsA("Frame") then
@@ -306,7 +310,7 @@ function Lib:CreateMain(Name)
 					end
 				end
 			end
-			
+
 			for i,v in pairs(TabHolder:GetChildren()) do
 				if v.Name == Name then
 					v.Visible = true
@@ -316,7 +320,7 @@ function Lib:CreateMain(Name)
 				end
 			end
 		end)
-		
+
 		local Elements = {}
 		function Elements:Button(Text,Call)
 			local SpaceBetween = Instance.new("TextLabel")
@@ -330,14 +334,14 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextColor3 = Color3.fromRGB(0, 0, 0)
 			SpaceBetween.TextSize = 14.000
 			SpaceBetween.Text = ""
-			
+
 			local ButtonElement = Instance.new("Frame")
 			local ElementUiCorne_2 = Instance.new("UICorner")
 			local ToggleText_2 = Instance.new("TextLabel")
 			local UIPadding_2 = Instance.new("UIPadding")
 			local ImageLabel = Instance.new("ImageLabel")
 			local ButtonElementTrigger = Instance.new("TextButton")
-			
+
 			ButtonElement.Name = "ButtonElement"
 			ButtonElement.Parent = TabHolderTabElement
 			ButtonElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -387,31 +391,31 @@ function Lib:CreateMain(Name)
 			ButtonElementTrigger.MouseLeave:Connect(function()
 				ButtonElement.BackgroundColor3 = Color3.fromRGB(63,63,63)
 			end)
-			
+
 			ButtonElementTrigger.MouseButton1Click:Connect(function()
 				local success, error = pcall(Call)
-				
+
 				if success then				
 					local goal = {}
 					goal.ImageColor3 = Color3.fromRGB(29, 255, 67)
-					
+
 					local goal2 = {}
 					goal2.ImageColor3 = Color3.fromRGB(255, 255, 255)
-					
-					
+
+
 					local tweeninfo = TweenInfo.new(0.3)
-					
+
 					local tweenGiveColor = tweenservice:Create(ImageLabel, tweeninfo, goal)
 					local tweenNormalColor = tweenservice:Create(ImageLabel, tweeninfo, goal2)
-					
+
 					tweenGiveColor:Play()
-					
+
 					tweenGiveColor.Completed:Connect(function()
 						tweenNormalColor:Play()
 					end)
 				else
 					warn(error)
-					
+
 					local goal = {}
 					goal.ImageColor3 = Color3.fromRGB(255, 29, 67)
 
@@ -432,7 +436,7 @@ function Lib:CreateMain(Name)
 				end
 			end)
 		end
-		
+
 		function Elements:Label(Text)
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -445,13 +449,13 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextColor3 = Color3.fromRGB(0, 0, 0)
 			SpaceBetween.TextSize = 14.000
 			SpaceBetween.Text = ""
-			
+
 			local LabelElement = Instance.new("Frame")
 			local ElementUiCorne_4 = Instance.new("UICorner")
 			local LabelText = Instance.new("TextLabel")
 			local UIPadding_4 = Instance.new("UIPadding")
 			local LabelImage = Instance.new("ImageLabel")
-			
+
 			LabelElement.Name = "LabelElement"
 			LabelElement.Parent = TabHolderTabElement
 			LabelElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -487,7 +491,7 @@ function Lib:CreateMain(Name)
 			LabelImage.ImageTransparency = 0.700
 			LabelImage.ScaleType = Enum.ScaleType.Fit
 		end
-		
+
 		function Elements:TextBox(Text, Call)
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -500,16 +504,16 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextColor3 = Color3.fromRGB(0, 0, 0)
 			SpaceBetween.TextSize = 14.000
 			SpaceBetween.Text = ""
-			
+
 			local TextBoxElement = Instance.new("Frame")
 			local ElementUiCorne_3 = Instance.new("UICorner")
 			local TextBoxImage = Instance.new("ImageLabel")
 			local ToggleText_3 = Instance.new("TextBox")
 			local UIPadding_3 = Instance.new("UIPadding")
-			
-			
-			
-			
+
+
+
+
 			TextBoxElement.Name = "TextBoxElement"
 			TextBoxElement.Parent = TabHolderTabElement
 			TextBoxElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -547,16 +551,16 @@ function Lib:CreateMain(Name)
 
 			UIPadding_3.Parent = ToggleText_3
 			UIPadding_3.PaddingLeft = UDim.new(0.0199999996, 0)
-			
+
 			ToggleText_3.FocusLost:Connect(function(EnterPressed)
 				if EnterPressed then
 					pcall(Call, ToggleText_3.Text)
 					ToggleText_3.Text = ""
 				end
 			end)
-			
+
 		end
-		
+
 		function Elements:Slider(Text,minValue, maxValue, Call)
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -569,7 +573,7 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextColor3 = Color3.fromRGB(0, 0, 0)
 			SpaceBetween.TextSize = 14.000
 			SpaceBetween.Text = ""
-			
+
 			local SliderElement = Instance.new("Frame")
 			local UICorner = Instance.new("UICorner")
 			local SliderBar = Instance.new("TextButton")
@@ -579,10 +583,10 @@ function Lib:CreateMain(Name)
 			local SliderValueName = Instance.new("TextLabel")
 			local SliderValueMax = Instance.new("TextLabel")
 			local SliderValueMin = Instance.new("TextLabel")
-			
-			
-			
-			
+
+
+
+
 			SliderElement.Name = "SliderElement"
 			SliderElement.Parent = TabHolderTabElement
 			SliderElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -652,14 +656,14 @@ function Lib:CreateMain(Name)
 			SliderValueMin.TextSize = 14.000
 			SliderValueMin.TextWrapped = true
 			SliderValueMin.TextXAlignment = Enum.TextXAlignment.Left
-			
-			
-			
+
+
+
 			local oldText = Text
-			
+
 			local Value
 			print(SliderBar.Size.X.Scale)
-			
+
 			SliderBar.MouseButton1Down:Connect(function()
 				Value = math.floor((((tonumber(maxValue) - tonumber(minValue)) / 550) * RealSlider.AbsoluteSize.X) + tonumber(minValue)) or 0
 				pcall(function()
@@ -668,7 +672,7 @@ function Lib:CreateMain(Name)
 				RealSlider.Size = UDim2.new(0, math.clamp(mouse.X - RealSlider.AbsolutePosition.X, 0, 550), 1, 0)
 				print(mouse.X, mouse.Y)
 				moveconnection = mouse.Move:Connect(function()
-					
+
 					Value = math.floor((((tonumber(maxValue) - tonumber(minValue)) / 550) * RealSlider.AbsoluteSize.X) + tonumber(minValue))
 					pcall(function()
 						Call(Value)
@@ -687,9 +691,9 @@ function Lib:CreateMain(Name)
 					end
 				end)
 			end)
-			
+
 		end
-		
+
 		function Elements:Toggle(Text,Call)
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -702,7 +706,7 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextColor3 = Color3.fromRGB(0, 0, 0)
 			SpaceBetween.TextSize = 14.000
 			SpaceBetween.Text = ""
-			
+
 			local ToggleElement = Instance.new("Frame")
 			local ElementUiCorne = Instance.new("UICorner")
 			local ToggleMain = Instance.new("Frame")
@@ -711,7 +715,7 @@ function Lib:CreateMain(Name)
 			local UICorner_4 = Instance.new("UICorner")
 			local ToggleText = Instance.new("TextLabel")
 			local UIPadding = Instance.new("UIPadding")
-			
+
 			ToggleElement.Name = "ToggleElement"
 			ToggleElement.Parent = TabHolderTabElement
 			ToggleElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -762,7 +766,7 @@ function Lib:CreateMain(Name)
 
 			UIPadding.Parent = ToggleText
 			UIPadding.PaddingLeft = UDim.new(0.0199999996, 0)
-			
+
 			local on = false
 			ToggleButton.MouseButton1Click:Connect(function()
 				if on == false then
@@ -789,9 +793,9 @@ function Lib:CreateMain(Name)
 					pcall(Call, on)
 				end
 			end)
-			
+
 		end
-		
+
 		function Elements:DestroyButton()
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -827,7 +831,7 @@ function Lib:CreateMain(Name)
 			UICorner.CornerRadius = UDim.new(0, 10)
 			UICorner.Parent = ButtonElement
 		end
-		
+
 		function Elements:Section(Text)
 			local SpaceBetween = Instance.new("TextLabel")
 			SpaceBetween.Name = "SpaceBetween"
@@ -841,15 +845,15 @@ function Lib:CreateMain(Name)
 			SpaceBetween.TextScaled = true
 			SpaceBetween.TextXAlignment = Enum.TextXAlignment.Left
 			SpaceBetween.Text = ""
-			
-			
+
+
 			local SectionElement = Instance.new("Frame")
 			local ElementUiCorne = Instance.new("UICorner")
 			local LabelText = Instance.new("TextLabel")
 			local UIPadding = Instance.new("UIPadding")
 			local LabelImage = Instance.new("ImageLabel")
 			local UIStroke = Instance.new("UIStroke")
-			
+
 			SectionElement.Name = "SectionElement"
 			SectionElement.Parent = TabHolderTabElement
 			SectionElement.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
@@ -859,7 +863,7 @@ function Lib:CreateMain(Name)
 			ElementUiCorne.CornerRadius = UDim.new(0, 10)
 			ElementUiCorne.Name = "ElementUiCorne"
 			ElementUiCorne.Parent = SectionElement
-			
+
 			UIStroke.Thickness = 2.8
 			UIStroke.Color = Color3.fromRGB(63,63,63)
 			UIStroke.Parent = SectionElement
@@ -889,25 +893,22 @@ function Lib:CreateMain(Name)
 			LabelImage.Image = "rbxassetid://7072980642"
 			LabelImage.ImageTransparency = 0.700
 			LabelImage.ScaleType = Enum.ScaleType.Fit
-		
-			
-			
+
+
+
 		end
-		
+
 		return Elements
 	end
-	
+
 	if game.CoreGui:FindFirstChild("MadUI") then
 		game.CoreGui.MadUI:Destroy()
 	end
-	
+
 	MadUI.Parent = game.CoreGui
-	
-	
+
+
 	return Tab
 end
-
-
-
 Init()
 return Lib
